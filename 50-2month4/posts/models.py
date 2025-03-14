7f4fa41fc8959912cf2377fe433bb1d61c0aceb5
marselev ilgiz <marselevilgiz56@gmail.com>
2025-03-14 06:21:28 +0600

    second hm

--- 50-2month4/posts/models.py
+++ 50-2month4/posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+"""
+SELECT * FROM posts; ---> posts = Post.objects.all();
+
+SELECT * FROM posts WHERE rate > 2; ---> posts = Post.objects.filter(rate_gt=2)
+
+SELECT * FROM posts WHERE id=1; ---> posts = Post.objects.get(id=1);
+"""
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=156)
+    content = models.CharField(max_length=356)
+    rate = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True,null=True)
+    updated_at = models.DateTimeField(auto_now=True,null=True)
+
+
+    def __str__(self):
+        return f"{self.title} {self.content}"
+
--- 50-2month4/posts/views.py
+++ 50-2month4/posts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import random
+from posts.models import Post
 
 def test_view(request):
     return HttpResponse(f"Hello, World ! {random.randint(1,100)}")
 
 
-def html_view(request):
-    return render(request, "posts/test.html")
+def home_page_view(request):
+    return render(request, "base.html")
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    return render(request, "posts/post_list.html", {"posts": posts})
 
